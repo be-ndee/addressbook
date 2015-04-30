f897ba986ddb1968b93656fc7662f923515cff73
Jacob <j@c.ob>
2015-04-30 11:12:25 +0200

    AddressList sql query implementation, not yet finished

--- WEB-INF/classes/AddressList.java
+++ WEB-INF/classes/AddressList.java
@@ -1,3 +1,36 @@
+import java.lang.Exception;
+import java.sql.*;
+
+
 public class AddressList {
-    
+
+    private ArrayList<Address>;
+
+    private getAll(){
+            // Class.forName("oracle.jdbc.driver.OracleDriver").newInstance();
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/addressbook?user=addressbook&password=addressbook");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM address;");
+            while (rs.next()) {
+                Address temp = new Address();
+                temp.setName = rs.getString("christianname");
+                temp.setName = rs.getString("name");
+                temp.setName = rs.getString("christianname");
+                temp.setName = rs.getString("christianname");
+                temp.setName = rs.getString("christianname");
+                temp.setName = rs.getString("christianname");
+                temp.setName = rs.getString("christianname");
+                temp.setName = rs.getString("christianname");
+                temp.setName = rs.getString("christianname");
+                temp.setName = rs.getString("christianname");
+                temp.setName = rs.getString("christianname");
+                temp.setName = rs.getString("christianname");
+
+                out.println(rs.getString("name"));
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+    }
 }
